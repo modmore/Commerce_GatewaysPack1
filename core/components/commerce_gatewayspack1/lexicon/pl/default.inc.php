d4ad7ba8e301ba389b7be6737bf9f08b9ad91540
Mark Hamstra <hello@markhamstra.com>
2018-01-30 17:23:21 +0100

    Translation sync

---
+++ core/components/commerce_gatewayspack1/lexicon/pl/default.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+$_lang['commerce_gatewayspack1'] = 'GatewaysPack1';
+$_lang['commerce_gatewayspack1.description'] = 'Adds additional payment gateways.';
+
+// Potentially shared keys
+$_lang['commerce_gatewayspack1.secret'] = 'Secret';
+
+/* Adyen Hosted Payment Page */
+$_lang['commerce_gatewayspack1.adyenhpp.secret_desc'] = 'The HMAC Key for the chosen platform (test or live) as managed in the Adyen dashboard under Skins.';
+$_lang['commerce_gatewayspack1.adyenhpp.merchant_account'] = 'Merchant Account';
+$_lang['commerce_gatewayspack1.adyenhpp.merchant_account_desc'] = 'The name of the merchant account the skin is enabled for. This can be seen when editing a skin in the Adyen dashboard, under the "Valid Accounts" header.';
+$_lang['commerce_gatewayspack1.adyenhpp.skin_code'] = 'Skin Code';
+$_lang['commerce_gatewayspack1.adyenhpp.skin_code_desc'] = 'The unique code of the skin you\'d like to use.';
