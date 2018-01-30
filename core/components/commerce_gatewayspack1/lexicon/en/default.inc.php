6dca37a53030a99cd4caaeae7a9ff0624fb21ebe
Mark Hamstra <hello@markhamstra.com>
2018-01-30 17:15:57 +0100

    Add missing lexicons

--- core/components/commerce_gatewayspack1/lexicon/en/default.inc.php
+++ core/components/commerce_gatewayspack1/lexicon/en/default.inc.php
@@ -7,8 +7,8 @@ $_lang['commerce_gatewayspack1.description'] = 'Adds additional payment gateways
 $_lang['commerce_gatewayspack1.secret'] = 'Secret';
 
 /* Adyen Hosted Payment Page */
-$_lang['commerce_gatewayspack1.adyenhpp.secret_desc'] = 'Secret';
+$_lang['commerce_gatewayspack1.adyenhpp.secret_desc'] = 'The HMAC Key for the chosen platform (test or live) as managed in the Adyen dashboard under Skins.';
 $_lang['commerce_gatewayspack1.adyenhpp.merchant_account'] = 'Merchant Account';
-$_lang['commerce_gatewayspack1.adyenhpp.merchant_account_desc'] = 'Merchant Account';
+$_lang['commerce_gatewayspack1.adyenhpp.merchant_account_desc'] = 'The name of the merchant account the skin is enabled for. This can be seen when editing a skin in the Adyen dashboard, under the "Valid Accounts" header.';
 $_lang['commerce_gatewayspack1.adyenhpp.skin_code'] = 'Skin Code';
-$_lang['commerce_gatewayspack1.adyenhpp.skin_code_desc'] = 'Skin Code';
+$_lang['commerce_gatewayspack1.adyenhpp.skin_code_desc'] = 'The unique code of the skin you\'d like to use.';
