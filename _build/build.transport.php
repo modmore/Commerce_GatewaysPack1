d8ba07e222eeecfedf9275eb6650f69bbdd4be0a
Mark Hamstra <hello@markhamstra.com>
2018-08-28 15:14:31 +0200

    Fix missing assets folder

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -85,10 +85,10 @@ $vehicle = $builder->createVehicle($namespace, $attributes);
 // Add the validator to check server requirements
 $vehicle->validate('php', array('source' => $sources['validators'] . 'requirements.script.php'));
 
-//$vehicle->resolve('file',array(
-//    'source' => $sources['source_assets'],
-//    'target' => "return MODX_ASSETS_PATH . 'components/';",
-//));
+$vehicle->resolve('file',array(
+    'source' => $sources['source_assets'],
+    'target' => "return MODX_ASSETS_PATH . 'components/';",
+));
 $vehicle->resolve('file',array(
     'source' => $sources['source_core'],
     'target' => "return MODX_CORE_PATH . 'components/';",
