90df184b6e286419720fe90a505d6e180219601c
Mark Hamstra <hello@markhamstra.com>
2019-01-11 23:28:06 +0100

    Register the composer.lock to add installed packages to the new configuration > about page

--- core/components/commerce_gatewayspack1/src/Modules/GatewaysPack1.php
+++ core/components/commerce_gatewayspack1/src/Modules/GatewaysPack1.php
@@ -1,5 +1,8 @@
 <?php
 namespace modmore\Commerce\GatewaysPack1\Modules;
+use modmore\Commerce\Admin\Configuration\About\ComposerPackages;
+use modmore\Commerce\Admin\Sections\SimpleSection;
+use modmore\Commerce\Events\Admin\PageEvent;
 use modmore\Commerce\Events\Gateways;
 use modmore\Commerce\GatewaysPack1\Gateways\AdyenHPP;
 use modmore\Commerce\Modules\BaseModule;
@@ -39,10 +42,31 @@ class GatewaysPack1 extends BaseModule {
         $loader->addLoader(new FilesystemLoader($root . '/templates/'));
 
         $dispatcher->addListener(\Commerce::EVENT_GET_PAYMENT_GATEWAYS, [$this, 'registerGateways']);
+
+        // Added in 0.12.0
+        if (defined('\Commerce::EVENT_DASHBOARD_LOAD_ABOUT')) {
+            $dispatcher->addListener(\Commerce::EVENT_DASHBOARD_LOAD_ABOUT, [$this, 'addLibrariesToAbout']);
+        }
     }
 
     public function registerGateways(Gateways $event)
     {
         $event->addGateway(AdyenHPP::class, 'Adyen');
+    }
+
+    public function addLibrariesToAbout(PageEvent $event)
+    {
+        $lockFile = dirname(dirname(__DIR__)) . '/composer.lock';
+        if (file_exists($lockFile)) {
+            $section = new SimpleSection($this->commerce);
+            $section->addWidget(new ComposerPackages($this->commerce, [
+                'lockFile' => $lockFile,
+                'heading' => $this->adapter->lexicon('commerce.about.open_source_libraries') . ' - ' . $this->adapter->lexicon('commerce_gatewayspack1'),
+                'introduction' => '', // Could add information about how libraries are used, if you'd like
+            ]));
+
+            $about = $event->getPage();
+            $about->addSection($section);
+        }
     }
 }
